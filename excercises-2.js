000731a5498920a8597172cdcfb46d3666031eae
Alexander Gonzalez <julian1alex@gmail.com>
2019-10-09 08:58:55 -0700

    stuff

--- excercises-2.js
+++ excercises-2.js
@@ -62,29 +62,29 @@ function isVowel(char) {
     }
 }
 
-// // --------------------- 
-// // Write a function rovarspraket() that will translate 
-// // a text into "Rovarspraket". That is, double every 
-// // consonant and place an occurrence of "o" in between. 
-// // For example, translate("this is fun") should return 
-// // the string "tothohisos isos fofunon".
-// // ---------------------
+// --------------------- 
+// Write a function rovarspraket() that will translate 
+// a text into "Rovarspraket". That is, double every 
+// consonant and place an occurrence of "o" in between. 
+// For example, translate("this is fun") should return 
+// the string "tothohisos isos fofunon".
+// ---------------------
 
-// console.assert(rovarspraket("a") === "a")
-// console.assert(rovarspraket("b") === "bob")
-// console.assert(rovarspraket("cat") === "cocatot")
-// console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
-// console.assert(rovarspraket(0) === "0")
+console.assert(rovarspraket("a") === "a")
+console.assert(rovarspraket("b") === "bob")
+console.assert(rovarspraket("cat") === "cocatot")
+console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
+console.assert(rovarspraket(0) === "0")
 
-// /* try to understand the loop, google translate - for loop*/
-
-
-// // ---------------------
-// // Define a function reverse() that computes the reversal 
-// // of a string. For example, reverse("jag testar") should 
-// // return the string "ratset gaj".
-// // ---------------------
+/* try to understand the loop, google translate - for loop*/
 
 
-// console.assert(reverse("books") === "skoob")
-// console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
+// ---------------------
+// Define a function reverse() that computes the reversal 
+// of a string. For example, reverse("jag testar") should 
+// return the string "ratset gaj".
+// ---------------------
+
+
+console.assert(reverse("books") === "skoob")
+console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
